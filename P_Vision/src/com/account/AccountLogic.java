073874cadd650996e8582ce87366e4e4860082d5
choihw0502 <48195155+choihw0502@users.noreply.github.com>
2019-03-20 21:31:11 +0900

    1 test

--- P_Vision/src/com/account/AccountLogic.java
+++ P_Vision/src/com/account/AccountLogic.java
@@ -5,11 +5,12 @@ import java.util.Map;
 
 public class AccountLogic {
 	AccountDao aDao = new AccountDao();
-	public List<Map<String, Object>> accountList() {//계좌리스트
-		List<Map<String,Object>> accountList = null;
-		accountList = aDao.accountList();
-		return accountList;
-	}
+//	test용 1
+//	public List<Map<String, Object>> accountList() {//계좌리스트
+//		List<Map<String,Object>> accountList = null;
+//		accountList = aDao.accountList();
+//		return accountList;
+//	}
 	public List<Map<String, Object>> accountAdd() {//계좌추가
 		List<Map<String,Object>> accountAdd = null;
 		accountAdd = aDao.accountAdd();
